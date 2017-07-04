57ffbf64322ecbd6f07c284432e70079def8ec77
Manuel <prezcabellomanuel@gmail.com>
2017-07-04 23:33:24 +0000

    Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,14 +9,6 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-class Employee < ActiveRecord::Base
-  validates :name, presence: true
-  validates :lastname, presence: true
-  validates :hourly_rate, :inclusion => {:in => [40,200]}
-  validates :store, presence: true
-
-end
-
 puts "What's the store name?"
 store_name = $stdin.gets.chomp
 
